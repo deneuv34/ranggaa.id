cc180b0250cb2e581e53ef4f719a8aad0532773c
Rangga Adhitya <deneuv3.4@gmail.com>
2021-12-09 10:47:12 +0700

    chore: update nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,14 +1,14 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'personal-project',
+    title: 'Welcome!',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Rangga Adhitya Prawira' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
